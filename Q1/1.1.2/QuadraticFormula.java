7b3c8eda7150c36ac3136ce879d81de3f3c5bd85
Mustan Pathetic <patheticmustan@gmail.com>
2020-09-15 18:21:26 -0400

    quick quick quick

--- Q1/1.1.2/QuadraticFormula.java
+++ Q1/1.1.2/QuadraticFormula.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
+import java.lang.Math;
 
+// I should have padded/floored numbers to the 4th place but whatever
+// 1         --> 1.0000
+// 1.2345678 --> 1.2345
 public class QuadraticFormula {
   public static void main(String[] args) {
     // create scanner for user input
@@ -15,8 +19,39 @@ public class QuadraticFormula {
     System.out.println("Enter C: ");
     double c = sc.nextDouble();
     
+    // we have a penapple
     // x = (-b +- sqrt((b*b) - (4*a*c))) / (2 * a)
+    
+    // HUUUU ("UH" backwards)
+    // pen
     // (-b + sqrt((b*b) - (4*a*c))) / (2 * a)
+    // apple
     // (-b - sqrt((b*b) - (4*a*c))) / (2 * a)
+    
+    // check if the discrim is <0
+    
+    double discrim = (b*b) - (4*a*c);
+    
+    if (discrim < 0) { // we're going to have complex answers
+        // haha cheese go pppffffft
+        // +"" is the fastest way to convert the double into a string
+        String numberPart = (-b / (2 * a)) + "";
+        String complexPart = ((Math.sqrt(discrim * -1) / (2 * a))+ "i");
+        
+        System.out.println(
+            "Solutions are " +
+            (numberPart + " + " + complexPart) + // I really wish Java had something like template literals
+            " and " +
+            (numberPart + " - " + complexPart)
+        );
+    } else { // normal answers, thank you very much
+        // stretch goal: 
+        System.out.println(
+            "Solutions are " +
+            ((-b + Math.sqrt((b*b) - (4*a*c))) / (2 * a)) +
+            " and " +
+            ((-b - Math.sqrt((b*b) - (4*a*c))) / (2 * a))
+        );
+    }
   }
 }

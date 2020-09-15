a0e16098bdfb00128d1a8c6098d528dca34ed634
Mustan Pathetic <patheticmustan@gmail.com>
2020-09-15 18:28:10 -0400

    oops tabs

--- Q1/1.1.2/QuadraticFormula.java
+++ Q1/1.1.2/QuadraticFormula.java
@@ -5,52 +5,52 @@ import java.lang.Math;
 // 1         --> 1.0000
 // 1.2345678 --> 1.2345
 public class QuadraticFormula {
-  public static void main(String[] args) {
-    // create scanner for user input
-    Scanner sc = new Scanner(System.in);
-    
-    // get input (a, b, c)
-    System.out.println("Enter A: ");
-    double a = sc.nextDouble();
-    
-    System.out.println("Enter B: ");
-    double b = sc.nextDouble();
-    
-    System.out.println("Enter C: ");
-    double c = sc.nextDouble();
-    
-    // we have a penapple
-    // x = (-b +- sqrt((b*b) - (4*a*c))) / (2 * a)
-    
-    // HUUUU ("UH" backwards)
-    // pen
-    // (-b + sqrt((b*b) - (4*a*c))) / (2 * a)
-    // apple
-    // (-b - sqrt((b*b) - (4*a*c))) / (2 * a)
-    
-    // check if the discrim is <0
-    
-    double discrim = (b*b) - (4*a*c);
-    
-    if (discrim < 0) { // we're going to have complex answers
-        // haha cheese go pppffffft
-        // +"" is the fastest way to convert the double into a string
-        String numberPart = (-b / (2 * a)) + "";
-        String complexPart = ((Math.sqrt(discrim * -1) / (2 * a))+ "i");
+    public static void main(String[] args) {
+        // create scanner for user input
+        Scanner sc = new Scanner(System.in);
         
-        System.out.println(
-            "Solutions are " +
-            (numberPart + " + " + complexPart) + // I really wish Java had something like template literals
-            " and " +
-            (numberPart + " - " + complexPart)
-        );
-    } else { // normal answers, thank you very much
-        System.out.println(
-            "Solutions are " +
-            ((-b + Math.sqrt((b*b) - (4*a*c))) / (2 * a)) +
-            " and " +
-            ((-b - Math.sqrt((b*b) - (4*a*c))) / (2 * a))
-        );
+        // get input (a, b, c)
+        System.out.println("Enter A: ");
+        double a = sc.nextDouble();
+        
+        System.out.println("Enter B: ");
+        double b = sc.nextDouble();
+        
+        System.out.println("Enter C: ");
+        double c = sc.nextDouble();
+        
+        // we have a penapple
+        // x = (-b +- sqrt((b*b) - (4*a*c))) / (2 * a)
+        
+        // HUUUU ("UH" backwards)
+        // pen
+        // (-b + sqrt((b*b) - (4*a*c))) / (2 * a)
+        // apple
+        // (-b - sqrt((b*b) - (4*a*c))) / (2 * a)
+        
+        // check if the discrim is <0
+        
+        double discrim = (b*b) - (4*a*c);
+        
+        if (discrim < 0) { // we're going to have complex answers
+            // haha cheese go pppffffft
+            // +"" is the fastest way to convert the double into a string
+            String numberPart = (-b / (2 * a)) + "";
+            String complexPart = ((Math.sqrt(discrim * -1) / (2 * a))+ "i");
+            
+            System.out.println(
+                "Solutions are " +
+                (numberPart + " + " + complexPart) + // I really wish Java had something like template literals
+                " and " +
+                (numberPart + " - " + complexPart)
+            );
+        } else { // normal answers, thank you very much
+            System.out.println(
+                "Solutions are " +
+                ((-b + Math.sqrt((b*b) - (4*a*c))) / (2 * a)) +
+                " and " +
+                ((-b - Math.sqrt((b*b) - (4*a*c))) / (2 * a))
+            );
+        }
     }
-  }
 }

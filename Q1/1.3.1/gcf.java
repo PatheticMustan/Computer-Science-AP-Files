24645a757ebfea23fc478e0520bc6a44c6985f69
Mustan Pathetic <patheticmustan@gmail.com>
2020-10-01 11:29:50 -0400

    added more comments for explanation

--- Q1/1.3.1/gcf.java
+++ Q1/1.3.1/gcf.java
@@ -7,7 +7,8 @@ public class gcf {
             {1, 350}, // 1
             {100, 8675309}, // 1
             {83, 83}, // 83
-            {64, 128} // din asked me for this test
+            {64, 128}, // din asked me for this test
+            {69, 420} // nice
         };
         
         for (int[] testCase : testCases) {
@@ -17,8 +18,22 @@ public class gcf {
     }
     
     public static int gcf(int a, int b) {
+        // "For the advanced version, your method should return the GCF, but should utilize the Euclidean Algorithm to do it."
         // https://en.wikipedia.org/wiki/Euclidean_algorithm
         // advanced version, after looking at the wikipedia page
+        
+        // ternary operators work like (condition? true : false). It's very useful for squishing things
+        // if (condition) {return trueThing} else {return falseThing}
+        // return condition? trueThing : falseThing
+        
+        // in this case, the full thing should look something like
+        // if (b == 0) {
+        //     return a;
+        // } else {
+        //     return gcf(b, a%b);
+        // }
+        
+        // after code go squishhhh...
         return b==0? a : gcf(b, a % b);
     }
 }
